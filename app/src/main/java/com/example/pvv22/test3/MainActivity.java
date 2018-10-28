e4e30f48442b8d3f30cbe607a7d946b46a238706
ellaktozar <pvv.222@gmail.com>
2018-10-28 14:45:36 +0300

    done without kludges

--- app/src/main/java/com/example/pvv22/test3/MainActivity.java
+++ app/src/main/java/com/example/pvv22/test3/MainActivity.java
@@ -15,34 +15,74 @@
 package com.example.pvv22.test3;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import java.net.InetAddress;
+import android.os.AsyncTask;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 /*
  * MainActivity class that loads {@link MainFragment}.
  */
 public class MainActivity extends Activity {
 
+    private TextView tw1, tw2;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tw1 = (TextView) findViewById(R.id.textView2);
-        netcode nc = new netcode();
-        tw1.setText(nc.getAddr());
+        tw1 = (TextView) findViewById(R.id.textView);
+        tw2 = (TextView) findViewById(R.id.textView2);
+        IpTask task1 = new IpTask();
+        task1.execute();
     }
 
     public void netButtonClick(View view) throws Exception {
-        netcode nc = new netcode();
-        nc.connect();
-        TextView tw = (TextView) findViewById(R.id.textView);
-        while (true){
-            if (nc.mesg != null)
-                tw.setText(nc.mesg);
+        NetTask task2 = new NetTask();
+        task2.execute();
+    }
+    class NetTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... params){
+            byte[] readBuf = new byte[1024];
+            byte[] sendBuf;
+            String mesg = null;
+            DatagramPacket recievePacket = new DatagramPacket(readBuf, readBuf.length);
+            DatagramSocket server = null;
+            try {
+                server = new DatagramSocket(1228);
+                server.receive(recievePacket);
+                mesg = new String(recievePacket.getData());
+                sendBuf = mesg.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, server.getInetAddress(), server.getPort());
+                server.send(sendPacket);
+            }
+            catch (Exception e){}
+            return mesg;
         }
-
+        @Override
+        protected void onPostExecute(String result){
+            tw1.setText(result);
+        }
+    }
+    class IpTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... params){
+            String ip = null;
+            try {
+                InetAddress addr = InetAddress.getLocalHost();
+                ip = addr.getHostAddress();
+            }
+            catch (Exception e){}
+            return ip;
+        }
+        @Override
+        protected void onPostExecute(String arg){
+            tw2.setText(arg);
+        }
     }
 }
