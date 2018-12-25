58d353f60db5474298f0dff90a69ab841282b18d
Evgeny Parenchenkov <19parenchenkov98@gmail.com>
2018-12-25 16:17:30 +0300

    Add manual text

--- app/src/main/java/com/petrsu/se/s2s/ManualActivity.java
+++ app/src/main/java/com/petrsu/se/s2s/ManualActivity.java
@@ -3,7 +3,9 @@ package com.petrsu.se.s2s;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class ManualActivity extends AppCompatActivity {
 
@@ -11,6 +13,8 @@ public class ManualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+        TextView man = (TextView) findViewById(R.id.textManualBody);
+        man.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void goToMain(View view) {
