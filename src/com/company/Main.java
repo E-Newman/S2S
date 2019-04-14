d572d1401a76f6ebd6725d65404575085fd368d4
Evgeny Parenchenkov <19parenchenkov98@gmail.com>
2019-04-14 12:05:19 +0300

    Interaction protocol in integers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,69 +18,74 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        byte[] receiveData = new byte[1000000];
-        byte[] sendData = new byte[1024];
-        InputStream ssIs = null;
-        OutputStream ssOs = null;
-        while(true) {
-            if (!gotConnect) {
-                try {
-                    Socket servS = serverSocket.accept();
-                    ssIs = servS.getInputStream();
-                    ssOs = servS.getOutputStream();
-                    ssIs.read(receiveData);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-                String sentence = new String(receiveData);
-                System.out.println("RECEIVED: " + sentence);
-                if (sentence.contains("Connect")) {
-                    String response = "Yes";
-                    sendData = response.getBytes();
+        byte[] receiveData = new byte[8192];
+        //byte[] sendData = new byte[1024];
+        int initCode = 0;
+        int i = 1;
+        DataInputStream dis1 = null;
+        DataOutputStream dos1 = null;
+        try {
+            Socket servS = serverSocket.accept();
+            dis1 = new DataInputStream(servS.getInputStream());
+            dos1 = new DataOutputStream(servS.getOutputStream());
+            System.out.println("here");
+            initCode = dis1.readInt();
+            System.out.println("here1");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("RECEIVED: " + initCode);
+        if (initCode == 1) {
+            //String response = "Yes";
+            //sendData = response.getBytes();
+            try {
+                dos1.writeInt(10);
+                dis1.close();
+                dos1.close();
+                Socket sockIs = imgSocket.accept();
+                DataInputStream dis =  new DataInputStream(sockIs.getInputStream());
+                while(true) {
+                    //DataOutputStream dos = null;
+                    System.out.println("Ready for img");
+                    File videoFile = null;
                     try {
-                        ssOs.write(sendData);
+                        videoFile = new File("C:/Users/EP/record" + i +".mp4");
+                        i++;
+                        //System.out.println("Video");
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
-                    gotConnect = true;
-                }
-            } else {
-                System.out.println("Ready for img");
-                File videoFile = null;
-                try {
-                    videoFile = new File("C:/Users/EP/record.mp4");
-                    if (videoFile.exists()) {
-                        videoFile.delete();
-                        videoFile.createNewFile();
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(videoFile);
+                        //System.out.println("Video write");
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
                     }
-                    //System.out.println("Video");
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    //byte[] videoBytes = null;
+                    try {
+                        int res = 0;
+                        System.out.println("Accepted");
+                        long filesize = dis.readLong();
+                        System.out.println("Размер файла " + filesize);
+                        while (filesize > 0 && (res = dis.read(receiveData, 0 , (int)Math.min(receiveData.length, filesize))) != -1){
+                            fos.write(receiveData, 0, res);
+                            System.out.println(res);
+                            filesize -= res;
+                            System.out.println(filesize);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.exit(0);
+                    }
+                    try {
+                        fos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-                FileOutputStream fos = null;
-                try {
-                    fos = new FileOutputStream(videoFile);
-                    //System.out.println("Video write");
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-                //byte[] videoBytes = null;
-               try {
-                   int res = 0;
-                   InputStream sockIs = imgSocket.accept().getInputStream();
-                   System.out.println("Accepted");
-                   while ((res = sockIs.read(receiveData)) != -1) {
-                       fos.write(receiveData, 0, res);
-                       System.out.println(res);
-                   }
-               } catch (Exception e) {
-                   e.printStackTrace();
-               }
-                try {
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
